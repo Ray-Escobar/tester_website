a2bd748ee1f528e1a5338bae43781fbb5b403a80
Pietro_V <pietrovigilanza7@gmail.com>
2018-10-16 13:43:41 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#A Website
+
+Yep it is just a website. Core HTML, some CSS and sprinkle that Javascript:
+
+- website
+-internet and things
---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/box-model.png') {
+      myImage.setAttribute ('src','images/jaysonTatum.jpg');
+    } else {
+      myImage.setAttribute ('src','images/box-model.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+
+
+
+/*document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}*/
